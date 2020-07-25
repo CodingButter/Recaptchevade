3f3831d4aad0bbc8623297c5f1a37dd13077128d
Jamie Nichols <Jamie337nichols@gmail.com>
2020-07-25 02:13:30 -0400

    fixed read me... Again

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # Recaptchevade
-node module to solve Google Recaptchas with 2captcha 
+
+node module to solve Google Recaptchas with 2captcha
 
 ## Sample usage
+
 ```js
 const puppeteer = require("puppeteer-core")
 const config = require("./Config.json")
-const solveCaptcha = require("./index.js")(config.TWO_CAPTCHA_KEY)
+const solveCaptcha = require("recaptchevade")(config.TWO_CAPTCHA_KEY)
 const browserOptions = {
-  headless: false
-}
-
-const main = async () => {
-  const browser = await puppeteer.connect(browserOptions)
+  headless: false,
+}(async () => {
+  const browser = await puppeteer.launch(browserOptions)
   const page = await browser.newPage()
   await page.goto("https://www.google.com/recaptcha/api2/demo")
   await solveCaptcha(page, null, async (response) => {
-    console.log(response);
-    document.querySelectore("#recaptcha-demo-submit").click();
+    document.querySelectore("#recaptcha-demo-submit").click()
   })
-}
-main()
+})()
 ```
--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 const Client = require("@infosimples/node_two_captcha")
 var _page
 var defaultClientOptions = {
-  timeout: 60000,
-  polling: 5000,
-  throwErrors: false,
+    timeout: 120000,
+    polling: 2000,
+    throwErrors: false
 }
 var twoCaptchaKey
 var clientOptions
 const getSiteKey = async (page) => {
-  await page.waitForSelector(".g-recaptcha")
-  return await page.evaluate(() => {
-    return document.querySelector(".g-recaptcha").getAttribute("data-sitekey")
-  })
+    await page.waitForSelector(".g-recaptcha")
+    return await page.evaluate(() => {
+        return document.querySelector(".g-recaptcha").getAttribute("data-sitekey")
+    })
 }
 
-const solveCaptcha = async function (page, options, submit) {
-  //Set client options and default client options
-  clientOptions = options || clientOptions
-  //Exit function if recaptcha is not found
-  if (!(await page.$(".g-recaptcha"))) return
+const solveCaptcha = async function (page, options, submit) { // Set client options and default client options
+    clientOptions = options || clientOptions
+    // Exit function if recaptcha is not found
+    if (!(await page.$(".g-recaptcha"))) 
+        return
 
-  //Initiciate a new 2Captcha client
-  const client = new Client(twoCaptchaKey, clientOptions)
 
-  const siteKey = await getSiteKey(page)
-  let response = await client.decodeRecaptchaV2({
-    googlekey: siteKey,
-    pageurl: page.url(),
-  })
+    
 
-  if (await response) {
-    submit(response.text)
-  }
+
+    // Initiciate a new 2Captcha client
+    const client = new Client(twoCaptchaKey, clientOptions)
+
+    const siteKey = await getSiteKey(page)
+    let response = await client.decodeRecaptchaV2({googlekey: siteKey, pageurl: page.url()})
+
+    if (await response) {
+        submit(response.text)
+    }
 }
 
 module.exports = (two_captcha_key, client_options) => {
-  defaultClientOptions = defaultClientOptions || client_options
-  twoCaptchaKey = two_captcha_key
-  return solveCaptcha
+    defaultClientOptions = defaultClientOptions || client_options
+    twoCaptchaKey = two_captcha_key
+    return solveCaptcha
 }
